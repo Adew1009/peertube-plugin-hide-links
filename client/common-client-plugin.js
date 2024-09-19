a256636da0e7e171c1a61b76b72fa4dab81adceb
Michael F Canzoneri <mcanz@technologytaskgroup.com>
2024-09-19 19:19:30 -0400

    Update common-client-plugin.js

--- client/common-client-plugin.js
+++ client/common-client-plugin.js
@@ -1,15 +1,13 @@
-// This function will be called by PeerTube to register the plugin
-async function register({ registerHook, peertubeHelpers }) {
+async function register ({ registerHook, peertubeHelpers }) {
   console.log("Plugin loaded and running");
 
-  // Fetch user quota asynchronously
-  async function fetchUserQuota() {
+  async function fetchUserQuota () {
     try {
-      const response = await fetch('/api/v1/users/me', {
+      const user = await fetch('/api/v1/users/me', {
         method: 'GET',
-        headers: peertubeHelpers.getAuthHeader(),
+        headers: peertubeHelpers.getAuthHeader()
       });
-      const userData = await response.json();
+      const userData = await user.json();
       console.log('User data fetched:', userData);
       return userData.videoQuota || 0;
     } catch (error) {
@@ -18,29 +16,19 @@ async function register({ registerHook, peertubeHelpers }) {
     }
   }
 
-  // Register the left-menu filter hook
   registerHook({
     target: 'filter:left-menu.links.create.result',
-    handler: async (menuLinks) => {
+    handler: async (result) => {
       console.log('Left menu links are being filtered...');
-
-      // Fetch the user quota
       const userQuota = await fetchUserQuota();
       console.log('User quota:', userQuota);
-
-      // If the user's upload quota is zero, filter out "Channels" and "Videos" links
       if (userQuota === 0) {
         console.log('Removing Channels and Videos links...');
-        return menuLinks.filter(link => link.name !== 'channels' && link.name !== 'videos');
+        return result.filter(link => !['Channels', 'Videos'].includes(link.key));
       }
-
-      // Return the unmodified menu links if quota is not zero
-      return menuLinks;
+      return result;
     }
   });
 }
 
-// Ensure the register function is globally available
-if (typeof window !== 'undefined') {
-  window.register = register;
-}
+export { register };
